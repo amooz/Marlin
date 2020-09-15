b05db6da5c766fe875ecff4e0a45af46ac810180
Adam Mooz <Adam@Mooz.ca>
2020-09-15 17:37:55 -0400

    Adjust probe offset, adjust lin_adv, add 2nd lin_adv

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -956,7 +956,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -20.6, 14.5, -3.13 }
+#define NOZZLE_TO_PROBE_OFFSET { -16.96, 11.03, -2.91}
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1517,8 +1517,8 @@
  */
 #define LIN_ADVANCE
 #if ENABLED(LIN_ADVANCE)
-  //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
-  #define LIN_ADVANCE_K 0.15    // Unit: mm compression per 1mm/s extruder speed
+  #define EXTRA_LIN_ADVANCE_K  0.07 // Enable for second linear advance constants
+  #define LIN_ADVANCE_K 0.11    // Unit: mm compression per 1mm/s extruder speed
   //#define LA_DEBUG            // If enabled, this will generate debug information output over USB.
 #endif
 
