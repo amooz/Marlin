60ae24301f033244e78c2e6abcba16fdfe9718a6
Adam Mooz <Adam@Mooz.ca>
2021-01-10 09:55:46 -0500

    Default z-offset to unconfigured for safety

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1077,7 +1077,7 @@
   #if HAS_BED_PROBE
     #define PROBE_OFFSET_WIZARD
     #if ENABLED(PROBE_OFFSET_WIZARD)
-      #define PROBE_OFFSET_START -3.0   // Estimated nozzle-to-probe Z offset, plus a little extra
+      #define PROBE_OFFSET_START -0   // Estimated nozzle-to-probe Z offset, plus a little extra
     #endif
   #endif
 
