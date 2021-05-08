62c2dd22fd39f57833695ec4e565fd2680f8fba4
Adam Mooz <Adam.Mooz@Shopify.com>
2021-05-08 08:35:12 -0400

    Animated icons

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.0.8 (v5)"
+#define SHORT_BUILD_VERSION "2.0.8 (v6)"
 
 /**
  * Verbose version identifier which should contain a reference to the location
